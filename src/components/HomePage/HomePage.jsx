f92c36461cd1ec814c63b07ab3cd36ef93c6c77c
andrew morales <andrew.morales725@gmail.com>
2021-12-16 11:33:35 -0500

    Added link buttons to homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <h1>HomePage</h1>
+        
+        <span>
+        <a class="btn btn-primary" href="/register" role="button">Sign up!</a>
+        <a class="btn btn-primary" href="/login" role="button">Sign In!</a>
+        </span>
     );
 }
  
