f28c3c4fc9ace670d6670e0a611bcb73ac61c5b6
andrew morales <andrew.morales725@gmail.com>
2021-12-16 12:41:33 -0500

    Added container to HomePage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        
-        <span>
-        <a class="btn btn-primary" href="/register" role="button">Sign up!</a>
-        <a class="btn btn-primary" href="/login" role="button">Sign In!</a>
-        </span>
+        <div class="container-fluid">
+            <span>
+                <a class="btn btn-primary" href="/register" role="button">Sign up!</a>
+                <a class="btn btn-primary" href="/login" role="button">Sign In!</a>
+            </span>
+        </div>
     );
 }
  
