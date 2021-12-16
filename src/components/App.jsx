a731ba714111cb50c57aba366dbe01b6cda3daca
andrew morales <andrew.morales725@gmail.com>
2021-12-16 11:34:22 -0500

    Added Login, RegisterUser components, Updated routes in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from './HomePage/HomePage';
+import RegisterUser from './RegisterUser/RegisterUser';
+import Login from './Login/Login';
 
 class App extends Component {
     constructor(props) {
@@ -14,6 +16,8 @@ class App extends Component {
             <Router>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
+                    <Route path="/register" element={<RegisterUser />} />
+                    <Route path="/login" element={<Login />} />
                 </Routes>
             </Router>
         );
---
+++ src/components/RegisterUser/RegisterUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RegisterUser = () => {
+    return ( 
+        <h1>Register User</h1>
+    );
+}
+ 
+export default RegisterUser;
