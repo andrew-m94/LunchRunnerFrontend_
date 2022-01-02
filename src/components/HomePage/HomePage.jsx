242f19187549da390f567cd37aaa9c02559e720e
andrew morales <andrew.morales725@gmail.com>
2022-01-02 16:38:01 -0500

    Added alerts for payment success/canceled

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const HomePage = () => {
+    const location = useLocation();
+
+    useEffect(() => {
+        const values = location.search;
+        const sp = new URLSearchParams(values);
+
+        if (sp.has("success") === true) {
+            alert('Order placed! You will recieve a confirmation email.')
+        }
+
+        if (sp.has("canceled") === true) {
+            alert('Order canceled -- proceed to checkout to complete your order.')
+        }
+    }, []);
+
     return ( 
         <div class="container-fluid">
             <span>
