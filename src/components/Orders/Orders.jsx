d2a638304a02239749688ae44ae5c890573f15a5
andrew morales <andrew.morales725@gmail.com>
2022-01-02 16:37:26 -0500

    Updated to properly send custom amount for payment charge

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -45,8 +45,8 @@ const Orders = (props) => {
                 <button type="button" class="btn btn-primary" onClick={(event) => navigate(`/create-order`)} >Add an Order!</button>
             
                 <section>
-                    <form action="http://localhost:8000/api/stripe/create-checkout-session" method="POST" >
-                        <input type="hidden" name="customAmount" value="200" />
+                    <form action="http://localhost:8000/api/stripe/create-checkout-session" method="POST">
+                        <input type="hidden" name="unit_amount" value={200} />
                         <button type="submit">
                         Checkout
                         </button>
