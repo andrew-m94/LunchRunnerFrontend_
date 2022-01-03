3435baeedcc28873e9ceb5a0b328741521042a28
andrew morales <andrew.morales725@gmail.com>
2022-01-03 12:52:29 -0500

    Updated styling

--- src/components/RegisterUser/RegisterUser.jsx
+++ src/components/RegisterUser/RegisterUser.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import './RegisterUser.css';
 
 const RegisterUser = () => {
     const [username, setUsername] = useState("");
@@ -27,30 +28,37 @@ const RegisterUser = () => {
     };
 
     return ( 
-        <div class="container-fluid">
-            <form onSubmit={(event) => handleSubmit(event)}>
-                <div class="form-group">
-                    <label for="usernameInput" >Username </label>
-                    <input type="username" class="form-control" id="usernameInput" placeholder="Username" onChange={(event) => setUsername(event.target.value)}/>
+        <div class="container-fluid register-user">
+            <div class="col "></div>
+            <div class="col ">
+                <form onSubmit={(event) => handleSubmit(event)}>
+                        <h1>Create Account</h1>
+                    <div class="form-group rfg">
+                        <label for="usernameInput" >Username </label>
+                        <input type="username" class="form-control" id="usernameInput" placeholder="Username" onChange={(event) => setUsername(event.target.value)}/>
+                        </div>
+                    <div class="form-group rfg">
+                        <label for="passwordInput" >Password</label>
+                        <input type="password" class="form-control" id="passwordInput" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
                     </div>
-                <div class="form-group">
-                    <label for="passwordInput" >Password</label>
-                    <input type="password" class="form-control" id="passwordInput" placeholder="Password" onChange={(event) => setPassword(event.target.value)}/>
-                </div>
-                <div class="form-group">
-                    <label for="emailInput" >Email </label>
-                    <input type="email" class="form-control" id="emailInput" placeholder="Email" onChange={(event) => setEmail(event.target.value)}/>
-                </div>
-                <div class="form-group">
-                    <label for="firstNameInput" >First Name </label>
-                    <input type="firstName" class="form-control" id="firstNameInput" placeholder="First Name" onChange={(event) => setFirstName(event.target.value)}/>
-                </div>
-                <div class="form-group">
-                    <label for="lastNameInput" >Last Name </label>
-                    <input type="lastName" class="form-control" id="lastNameInput" placeholder="Last Name" onChange={(event) => setLastName(event.target.value)}/>
-                </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+                    <div class="form-group rfg">
+                        <label for="emailInput" >Email </label>
+                        <input type="email" class="form-control" id="emailInput" placeholder="Email" onChange={(event) => setEmail(event.target.value)}/>
+                    </div>
+                    <div class="form-group rfg">
+                        <label for="firstNameInput" >First Name </label>
+                        <input type="firstName" class="form-control" id="firstNameInput" placeholder="First Name" onChange={(event) => setFirstName(event.target.value)}/>
+                    </div>
+                    <div class="form-group rfg">
+                        <label for="lastNameInput" >Last Name </label>
+                        <input type="lastName" class="form-control" id="lastNameInput" placeholder="Last Name" onChange={(event) => setLastName(event.target.value)}/>
+                    </div>
+                    <button type="submit" class="btn btn-danger">Submit</button>
+                </form>
+            </div>
+            <div class="col ">
+            <img class="img-fluid" id = "page-art" src="/images/running.png" alt="Cannot Be Displayed"/>
+            </div>
         </div>
     );
 }
