b03954db5b28b59abb30f3f2c160cff43c6d830d
andrew morales <andrew.morales725@gmail.com>
2022-01-03 12:28:57 -0500

    Updated page content, bootstrap styling

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import './LandingPage.css'
+import './LandingPage.css';
 
 const LandingPage = () => {
     return (
         <div class="container-fluid landing-page">
-            <span>
-                <a class="btn btn-primary" href="/register" role="button">Sign up!</a>
-                <a class="btn btn-primary" href="/login" role="button">Sign In!</a>
-            </span>
+            <div class="col info">
+                Sending an employee to pick up lunch? Make it easy!
+            </div>
+            <div class="col">
+                <div class="title fw-bold">
+                    LunchRunner
+                </div>
+                <div>
+                    <a class="btn btn-danger" href="/register" role="button">Sign up!</a>
+                    <a class="btn btn-danger" href="/login" role="button">Sign In!</a>
+                </div>
+            </div>
+            <div class="col">
+                <img class="img-fluid" id = "page-art" src="/images/running.png" alt="Cannot Be Displayed"/>
+            </div>
         </div>
     );
 }
