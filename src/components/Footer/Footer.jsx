bcf8912c8cdcc70022cb22ae194af8f22ad68937
andrew morales <andrew.morales725@gmail.com>
2022-01-04 09:43:16 -0500

    Updated link url

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <h4>
                 <div>Project created with React</div>
                 <div>1/4/2022</div>
-                <a class="footer-link" href="https://github.com/andrew-m94/ReactMusicLibrary_">Github Repository</a>
+                <a class="footer-link" href="https://github.com/andrew-m94/LunchRunnerFrontend_">Github Repository</a>
             </h4>
         </footer>
     )
